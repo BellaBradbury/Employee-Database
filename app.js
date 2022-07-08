761c7d01f65af7749b7fc7422af2a3122f9ba2a7
BellaBradbury <bellabradbury@yahoo.com>
2022-07-08 18:27:18 -0400

    index.html updated to recieve employee directory

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const url = "https://randomuser.me/api/1.4/?results=12&nat=us&inc=name,location,email,dob,cell,picture";
 
+
+// FETCHES 12 RANDOM UNITED STATES "EMPLOYEES"
+  // WITH FULL NAME, FULL LOCATION, EMIAL, DOB, CELL #, AND PICTURE
 function fetchData() {
   fetch(url)
     .then(checkStatus)
