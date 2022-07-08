5037e1d2ead40d96e1f6861ff6554ec7f631f6e9
BellaBradbury <bellabradbury@yahoo.com>
2022-07-08 18:03:49 -0400

    fetching data from randomuser.me

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-$.ajax({
-  url: 'https://randomuser.me/api/',
-  dataType: 'json',
-  success: function(data) {
-    console.log(data);
-  }
-});
+const url = "https://randomuser.me/api/1.4/?results=12&?nat=us&?inc=name,location,email,dob,cell,picture,nat";
+
+function fetchData() {
+  fetch(url)
+    .then(checkStatus)
+    .then( response => response.json() )
+    .catch( error => console.log('ERROR:', error) );
+}
+
+fetchData();
